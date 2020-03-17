903dd9cebce5cd6918133cd83e0816039a1deb26
jona1043 <jonatan1040@gmail.com>
2020-03-18 00:03:33 +0200

    Updates

---
+++ precache-manifest.6491dde43af727864f6ac90a7db7eddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d2d8a18788755bc4c5803df340cc72",
+    "url": "/react-monster-app/index.html"
+  },
+  {
+    "revision": "459201b5c6bd9a37cb07",
+    "url": "/react-monster-app/static/css/main.f2293f80.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/react-monster-app/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-monster-app/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459201b5c6bd9a37cb07",
+    "url": "/react-monster-app/static/js/main.51229068.chunk.js"
+  },
+  {
+    "revision": "68551dae55f6c499c2c3",
+    "url": "/react-monster-app/static/js/runtime-main.2fd075ee.js"
+  }
+]);
